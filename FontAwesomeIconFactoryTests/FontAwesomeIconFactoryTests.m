02bb7a33eede43695beb27cc158b721741d7ede6
Nikolaj Schumacher <git@nschum.de>
2013-06-17 18:43:27 +0200

    Add test for inset images.

--- FontAwesomeIconFactoryTests/FontAwesomeIconFactoryTests.m
+++ FontAwesomeIconFactoryTests/FontAwesomeIconFactoryTests.m
@@ -30,4 +30,19 @@
     }
 }
 
+- (void)testEdgeInsetsShouldIncreaseImageSize {
+    NIKFontAwesomeIconFactory *insetFactory = [_factory copy];
+    insetFactory.edgeInsets = (NIKEdgeInsets){1.0, 3.0, 5.0, 7.0};
+
+    for (NIKFontAwesomeIcon icon = NIKFontAwesomeIconGlass;
+         icon <= NIKFontAwesomeIconFolderOpenAlt;
+         icon++) {
+
+        NIKImage *image = [_factory createImageForIcon:icon];
+        NIKImage *insetImage = [insetFactory createImageForIcon:icon];
+        assertThatDouble(insetImage.size.height, equalToDouble(image.size.height + 6.0));
+        assertThatDouble(insetImage.size.width, equalToDouble(image.size.width + 10.0));
+    }
+}
+
 @end
