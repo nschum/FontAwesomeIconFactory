bd283f2fbab6842e2eb6b340a3e5b006e614b615
Nikolaj Schumacher <git@nschum.de>
2014-09-08 23:50:50 +0200

    Sort generated enum alphabetically.

--- build-header.py
+++ build-header.py
@@ -4,6 +4,15 @@
 # # vim: set fileencoding=utf-8 :
 
 import re
+
+def convert_symbol(name):
+  index = 0
+  while (index > -1):
+    name = name[:index] + name[index+1:index+2].upper()+ name[index+2:]
+    index = name.find('-', index+1)
+  return name
+
+
 head = "typedef enum NIKFontAwesomeIcon : UniChar {\n"
 code = "    NIKFontAwesomeIcon{} = 0x{},\n"
 foot = "} NIKFontAwesomeIcon;\n"
@@ -12,12 +21,10 @@ w.write(head)
 f = open('./Font-Awesome/scss/_variables.scss', 'r')
 rx = re.compile('^\$fa-var([\w\d\-]+): \"\\\\(f[\d\w]*)\";')
 lines = [l for l in f.read().split("\n") if rx.match(l)]
-for line in lines:
-	m = rx.findall(line)[0]
-	name = m[0]
-	index = 0
-	while (index > -1):
-		name = name[:index] + name[index+1:index+2].upper()+ name[index+2:]
-		index = name.find('-', index+1)
-	w.write(code.format(name, m[1]))
+pairs = [rx.findall(line)[0] for line in lines]
+symbol_pairs = sorted([(convert_symbol(pair[0]), pair[1]) for pair in pairs], key=lambda t: t[0])
+
+for symbol in symbol_pairs:
+  w.write(code.format(symbol[0], symbol[1]))
+
 w.write(foot)
