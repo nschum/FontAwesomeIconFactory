4fe1937eb2d6f6b57eecb4e621eb0f67f56fcc61
Nikolaj Schumacher <nik@Friday.local>
2015-04-24 21:13:01 +0200

    Add umbrella header.

---
+++ FontAwesomeIconFactory.h
@@ -0,0 +1,13 @@
+#import <FontAwesomeIconFactory/NIKFontAwesomeIcon.h>
+#import <FontAwesomeIconFactory/NIKFontAwesomeIconTraits.h>
+#import <FontAwesomeIconFactory/NIKFontAwesomeIconFactory.h>
+
+#import <FontAwesomeIconFactory/NIKFontAwesomeButton.h>
+#import <FontAwesomeIconFactory/NIKFontAwesomeImageView.h>
+
+#if TARGET_OS_IPHONE
+#import <FontAwesomeIconFactory/NIKFontAwesomeTabBarItem.h>
+#import <FontAwesomeIconFactory/NIKFontAwesomeIconFactory+iOS.h>
+#else
+#import <FontAwesomeIconFactory/NIKFontAwesomeIconFactory+OSX.h>
+#endif
